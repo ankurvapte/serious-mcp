390959b85f1d9474f66df6d4a60a5a1c215968b9
Ankur Apte <ankurapte@outlook.com>
2026-03-09 19:06:06 +0530

    Moved the AI API Key to environment variables.

--- agent.py
+++ agent.py
@@ -1,10 +1,10 @@
 import asyncio
+import os
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langgraph.prebuilt import create_react_agent
+#from langgraph.prebuilt import create_react_agent
 from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 server_params = StdioServerParameters(
@@ -12,7 +12,7 @@ server_params = StdioServerParameters(
                                         args=["sales_server.py"]
                                     )
 
-async def run_agent():
+async def run_gemini_agent():
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
@@ -21,12 +21,17 @@ async def run_agent():
             mcp_tools = await load_mcp_tools(session)
 
             # 4. Create LangGraph Agent
-            model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, google_api_key="AIzaSyDSXec1qp7h2BcYDWlwdtvGW958fx_o9NQ")
+            model = ChatGoogleGenerativeAI(
+                                        model="gemini-2.0-flash", 
+                                        temperature=0, 
+                                        google_api_key=os.getenv("GOOGLE_API_KEY"))
+            
             agent = create_agent(model=model, tools=mcp_tools)
 
             # 5. Execute a task
             query = "Compare the revenue of the North and West regions."
             print(f" --- Calling Gemini Agent : '{query}' --- ")
+
             result = await agent.ainvoke({
                 "messages": [("user", query)]
             })
@@ -36,4 +41,4 @@ async def run_agent():
 
 
 if __name__ == "__main__":
-    asyncio.run(run_agent())
+    asyncio.run(run_gemini_agent())
